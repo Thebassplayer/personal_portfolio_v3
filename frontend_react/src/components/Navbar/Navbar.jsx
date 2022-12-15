2c3b2c3f99a98eff783a7b3d187285c1852b6e6f
Roy Lopez <windup.dues-0f@icloud.com>
2022-12-15 16:38:05 -0300

    Navbar ready

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -16,28 +16,32 @@ const Navbar = () => {
         <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
-        {navbarButtons.map(item => (
+        {["home", "about", "work", "skills", "contact"].map(item => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
-      <div className="app__navbar-manu">
+
+      <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
+
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: "ease" }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
           >
             <HiX onClick={() => setToggle(false)} />
-            {navbarButtons.map(item => (
-              <li key={item}>
-                <a href={`#${item}`} onClick={() => setToggle(false)}>
-                  {item}
-                </a>
-              </li>
-            ))}
+            <ul>
+              {["home", "about", "work", "skills", "contact"].map(item => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </motion.div>
         )}
       </div>
