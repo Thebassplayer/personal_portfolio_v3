404c5fe6459953637ed81f656493bf05a0fcdfb6
Roy Lopez <windup.dues-0f@icloud.com>
2023-01-01 15:03:29 -0300

    skills section populated

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     "home",
     "about",
     "work",
-    "skill",
+    "skills",
     "testimonials",
     "contact",
   ];
--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -4,7 +4,7 @@ const navbarButtons = [
   "home",
   "about",
   "work",
-  "skill",
+  "skills",
   "testimonials",
   "contact",
 ];
