2be818be5da914e0de3b3e24f2f874181535b503
Roy Lopez <windup.dues-0f@icloud.com>
2023-01-05 14:22:56 -0300

    contact section populated

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,4 +1,4 @@
-import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { About, Contact, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 
 import "./App.scss";
@@ -12,7 +12,7 @@ function App() {
       <Work />
       <Skills />
       <Testimonial />
-      <Footer />
+      <Contact />
     </div>
   );
 }
---
+++ frontend_react/src/container/Contact/Contact.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { client } from "../../client";
+
+import "./Contact.scss";
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { name, email, message } = formData;
+  const handleChangeInput = e => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: "contact",
+      name: name,
+      email: email,
+      message: message,
+    };
+    client.create(contact).then(res => {
+      setLoading(false);
+      setIsFormSubmitted(true);
+    });
+  };
+  return (
+    <>
+      <h2 className="head-text">Contact me!</h2>
+      <div className="app__contact-cards">
+        <a href="mailto:roylopezdev@gmail.com" className="p-text">
+          <div className="app__contact-card">
+            <img src={images.email} alt="email" />
+            <span>roylopezdev@gmail.com</span>
+          </div>
+        </a>
+      </div>
+
+      {!isFormSubmitted ? (
+        <div className="app__contact-form app__flex">
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="text"
+              placeholder="Your name"
+              value={name}
+              onChange={handleChangeInput}
+              name="name"
+            />
+          </div>
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="email"
+              placeholder="Your email"
+              value={email}
+              onChange={handleChangeInput}
+              name="email"
+            />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            ></textarea>
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {loading ? "Sending..." : "Send Message"}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">Thanks for getting in touch</h3>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Contact, "app__contact"),
+  "contact",
+  "app__whitebg"
+);
